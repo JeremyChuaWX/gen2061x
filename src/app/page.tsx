626b3ce3c1f72e75a6cdf448777e2456de5b61e9
Jeremy Chua <chua.jeremy.wx@gmail.com>
2025-01-17 21:20:05 +0800

    feat: scheme filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,182 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useMemo } from "react";
+
+type Scheme = {
+    name: string;
+    eligibility: {
+        dob?: Date;
+        age?: number;
+        income?: number;
+    };
+    benefits: string;
+};
+
+const SCHEMES: Scheme[] = [
+    {
+        name: "Pioneer Generation Package",
+        eligibility: {
+            dob: new Date("12/31/1949"),
+        },
+        benefits:
+            "- MediSave top-ups\n- Additional outpaitent care subsidies\n- CareShield\n- Additional Outpatient Care Subsidies\n - CareShield Life Additional Participation Incentives\n- Special MediShield Life Premium Subsidies\n- Pioneer Generation Disability Assistance Scheme",
+    },
+    {
+        name: "Merdeka Generation Package",
+        eligibility: {
+            dob: new Date("12/31/1959"),
+        },
+        benefits:
+            "- Additional Outpatient Care Subsidies\n- CareShield Life Additional Participation Incentives\n- Additional MediShield Life Premium Subsidies",
+    },
+    {
+        name: "Lease Buyback Scheme",
+        eligibility: {
+            income: 14000,
+        },
+        benefits:
+            "- Sell part of your flat's lease to HDB and choose to retain the length of lease based on the age of the youngest owner\n- Proceeds from selling part of your flat's lease will be used to top up your CPF Retirement Account (RA), and you can use your CPF RA to join CPF LIFE, which will provide you with a monthly income for life",
+    },
+];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    const [dob, setDob] = useState(new Date());
+    const age = calculateAge(dob);
+    const [income, setIncome] = useState(0);
+    const schemes = useMemo(() => {
+        return SCHEMES.filter((scheme) => {
+            const {
+                dob: schemeDob,
+                age: schemeAge,
+                income: schemeIncome,
+            } = scheme.eligibility;
+            const isDobEligible =
+                !schemeDob || dob.getTime() <= schemeDob.getTime();
+            const isAgeEligible = !schemeAge || age >= schemeAge;
+            const isIncomeEligible = !schemeIncome || income <= schemeIncome;
+            console.log(scheme.name, dob, schemeDob, isDobEligible);
+            return isDobEligible && isAgeEligible && isIncomeEligible;
+        });
+    }, [age, dob, income]);
+    const onClick = () => {
+        alert("Filter by additional details not implemented.");
+    };
+    return (
+        <div className="flex flex-col py-8 mx-auto space-y-8 w-[540px]">
+            <div className="flex flex-col space-y-4">
+                <h1 className="text-lg font-bold">Senior Information</h1>
+                <div className="flex flex-col space-y-1">
+                    <label className="block text-sm font-medium text-gray-700">
+                        Date of birth
+                    </label>
+                    <input
+                        type="date"
+                        value={dob ? dob.toISOString().slice(0, 10) : ""}
+                        onChange={(e) =>
+                            setDob(
+                                e.target.value
+                                    ? new Date(e.target.value)
+                                    : new Date(),
+                            )
+                        }
+                        className="py-2 px-4 w-full rounded-lg border border-gray-300 shadow-sm transition duration-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                    />
+                    <label className="block text-sm font-medium text-gray-700">
+                        Age: {age}
+                    </label>
+                </div>
+                <div className="flex flex-col space-y-2">
+                    <label className="block text-sm font-medium text-gray-700">
+                        Monthly household income
+                    </label>
+                    <input
+                        type="number"
+                        value={income}
+                        onChange={(e) =>
+                            setIncome(
+                                e.target.value ? parseInt(e.target.value) : 0,
+                            )
+                        }
+                        className="py-2 px-4 w-full rounded-lg border border-gray-300 shadow-sm transition duration-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                    />
+                </div>
+                <div className="flex flex-col space-y-2">
+                    <label className="block text-sm font-medium text-gray-700">
+                        Additional details
+                    </label>
+                    <textarea
+                        placeholder="Enter additional details here to be parsed by AI to match against complex eligibility criteria of schemes."
+                        className="py-2 px-4 w-full rounded-lg border border-gray-300 shadow-sm transition duration-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none min-h-[200px]"
+                    />
+                    <button
+                        onClick={onClick}
+                        className="self-end py-2 px-4 font-medium text-white bg-blue-600 rounded-lg shadow-md transition duration-300 hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none w-fit"
+                    >
+                        Filter by additional details
+                    </button>
+                </div>
+            </div>
+            <div className="flex flex-col space-y-4">
+                <h1 className="text-lg font-bold">Schemes</h1>
+                <div className="flex flex-wrap gap-4 w-full">
+                    {schemes.length === 0 ? (
+                        <p className="w-full text-center text-gray-400">
+                            No schemes found
+                        </p>
+                    ) : (
+                        schemes.map((scheme, idx) => (
+                            <SchemeComponent scheme={scheme} key={idx} />
+                        ))
+                    )}
+                </div>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
+}
+
+function calculateAge(dob: Date): number {
+    const today = new Date();
+    let age = today.getFullYear() - dob.getFullYear();
+    const monthDifference = today.getMonth() - dob.getMonth();
+    if (
+        monthDifference < 0 ||
+        (monthDifference === 0 && today.getDate() < dob.getDate())
+    ) {
+        age--;
+    }
+    return age;
+}
+
+function SchemeComponent({ scheme }: { scheme: Scheme }) {
+    return (
+        <div className="flex flex-col p-4 space-y-1 rounded-lg border border-gray-200 shadow-md transition-shadow duration-300 hover:shadow-lg shadow-gray-400 w-[200px] h-[200px]">
+            <p className="font-bold">{scheme.name}</p>
+            <div>
+                <h2 className="text-sm">Eligibility</h2>
+                {scheme.eligibility.age !== undefined && (
+                    <p className="text-sm font-medium text-gray-700">
+                        Age &ge; {scheme.eligibility.age}
+                    </p>
+                )}
+                {scheme.eligibility.dob !== undefined && (
+                    <p className="text-sm font-medium text-gray-700">
+                        Date of birth &ge;{" "}
+                        {scheme.eligibility.dob.toISOString().slice(0, 10)}
+                    </p>
+                )}
+                {scheme.eligibility.income !== undefined && (
+                    <p className="text-sm font-medium text-gray-700">
+                        Monthly household income &le;{" "}
+                        {scheme.eligibility.income}
+                    </p>
+                )}
+            </div>
+            <div className="overflow-hidden">
+                <h2 className="text-sm">Benefits</h2>
+                <p className="text-sm font-medium text-gray-700 whitespace-pre-wrap">
+                    {scheme.benefits}
+                </p>
+            </div>
+        </div>
+    );
 }
